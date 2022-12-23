30eea1d794f4b2a0e584237895e3c1b8ed479071
kimminwoo <kbsserver@naver.com>
2022-12-24 00:19:53 +0900

    update

---
+++ 자주출제되는 기타알고리즘/N번째 큰 수.py
@@ -0,0 +1,27 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+n = int(input().strip())
+hq = []
+
+for _ in range(n):
+    print(hq)
+    nums=list(map(int,input().split()))
+    
+    if not hq:
+        for num in nums:
+            heapq.heappush(hq,num)
+    else:
+        
+        for num in nums:
+            if hq[0]<num:
+                heapq.heappush(hq,num)
+                heapq.heappop(hq)
+                
+
+print(hq[0])
+            
+        
+        
+        
